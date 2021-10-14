9b10294d6f03a18dbc4f1f7bde9bd277d45561e5
twiffydorax <45312658+01arelaVokribiS@users.noreply.github.com>
2021-10-14 14:23:24 +0300

    list component

---
+++ website/src/components/CustomList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container" v-if="users.length > 0">
+        <item-list
+            v-for="user in users"
+            :key="user.id"
+            :user="user"
+        />
+    </div>
+    <div v-else>Empty list</div>
+</template>
+
+<script>
+import ItemList from "@/components/ItemList";
+
+export default {
+    components: {
+        ItemList,
+    },
+    props: {
+        users: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
