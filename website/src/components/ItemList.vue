a6f5dad590dd6da9e68946756fd888e100ca19f9
twiffydorax <45312658+01arelaVokribiS@users.noreply.github.com>
2021-10-14 14:23:38 +0300

    component item list

---
+++ website/src/components/ItemList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="border rounded px-3 py-2 mb-1">
+        <div>Surname: {{ user.surname }}</div>
+        <div>Name: {{ user.name }}</div>
+        <div>Username: {{ user.username }}</div>
+        <div>Password: {{ user.password }}</div>
+        <div>Country: {{ user.country }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
