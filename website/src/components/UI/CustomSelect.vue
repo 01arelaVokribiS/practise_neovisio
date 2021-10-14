d4e7f21e2e4fc8f4e748907b9840f1a2fbd40fbf
twiffydorax <45312658+01arelaVokribiS@users.noreply.github.com>
2021-10-14 14:24:29 +0300

    reusable component select

---
+++ website/src/components/UI/CustomSelect.vue
@@ -0,0 +1,36 @@
+<template>
+    <select class="form-select" v-model="modelValue" @change="changeOption">
+        <option disabled value="">Select country</option>
+        <option
+            v-for="option in options"
+            :key="option.value" 
+            :value="option.value"
+        >
+            {{ option.name }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'custom-select',
+    props: {
+        modelValue: {
+            type: String
+        },
+        options: {
+            type: Array,
+            default: () => []
+        }
+    },
+    methods: {
+        changeOption(event) {
+            this.$emit('update:modelValue', event.target.value);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
