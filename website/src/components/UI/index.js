6a9a553358c744eb1ee7eadf9b6bf5fe92138cfa
twiffydorax <45312658+01arelaVokribiS@users.noreply.github.com>
2021-10-14 14:25:23 +0300

    point to register reusable components

---
+++ website/src/components/UI/index.js
@@ -0,0 +1,9 @@
+import CustomInput from "@/components/UI/CustomInput";
+import CustomModalWindow from "@/components/UI/CustomModalWindow";
+import CustomSelect from "@/components/UI/CustomSelect";
+
+export default [
+    CustomInput,
+    CustomModalWindow,
+    CustomSelect,
+]
