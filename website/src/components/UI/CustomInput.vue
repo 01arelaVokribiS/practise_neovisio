2da6863cb85b490216c0563bf0b0032f1f534c4e
twiffydorax <45312658+01arelaVokribiS@users.noreply.github.com>
2021-10-14 14:23:59 +0300

    reusable component input type='text'

---
+++ website/src/components/UI/CustomInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <input
+        :value="modelValue"
+        @input="updateInput"
+        type="text"
+        class="form-control"
+    >
+</template>
+
+<script>
+export default {
+    name: 'custom-input',
+    props: {
+        modelValue: [String, Number]
+    },
+    methods: {
+        updateInput(event) {
+            this.$emit('update:modelValue', event.target.value);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
