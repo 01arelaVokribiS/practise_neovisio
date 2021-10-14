26ee235443488aa385503e83e01c26aa42b7fbdb
twiffydorax <45312658+01arelaVokribiS@users.noreply.github.com>
2021-10-14 14:22:59 +0300

    main page

---
+++ website/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <!-- modal window -->
+    <custom-modal-window>
+        <!-- users list -->
+        <custom-list
+            :users="users"
+        />
+    </custom-modal-window>
+    <!-- form -->
+    <custom-form
+        @create="createUser"
+    />
+</template>
+
+<script>
+import CustomForm from "@/components/CustomForm";
+import CustomList from "@/components/CustomList";
+
+export default {
+    components: {
+        CustomForm,
+        CustomList,
+    },
+    data() {
+        return {
+            users: [
+                // { id: 1, surname: 'surname1', name: 'name1', username: 'username1', password: 'password1' },
+            ],
+        }
+    },
+    methods: {
+        createUser(user) {
+            this.users.push(user);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
